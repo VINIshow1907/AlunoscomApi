595a609dda70611cc4b81bbbd84516484a6807cc
VINIshow1907 <viniciuslucio792@gmail.com>
2024-04-30 20:06:44 -0300

    colocando o cor para fazer a ligação da api com o front-end

--- AlunoApi/AlunoApi/obj/Debug/net8.0/AlunoApi.AssemblyInfo.cs
+++ AlunoApi/AlunoApi/obj/Debug/net8.0/AlunoApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AlunoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+579a8cc4cccde58a97c5a40ec7b8e8f90d3e4cad")]
 [assembly: System.Reflection.AssemblyProductAttribute("AlunoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AlunoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
