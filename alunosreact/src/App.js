46564bd24008849a8c57bcd3e9d295eaa1bd26f5
VINIshow1907 <viniciuslucio792@gmail.com>
2024-04-30 22:26:46 -0300

    Aula-7. Foi adicionado a opção de editar o aluno. Criado = modaleditar, const selecionaraluno, abirfecharmodaleditar,pedidoput,

--- alunosreact/src/App.js
+++ alunosreact/src/App.js
@@ -11,6 +11,7 @@ function App() {
   
   const [data,setData]=useState([]);
   const [modalIncluir,setModalIncluir]=useState(false);
+  const [modalEditar,setModalEditar]=useState(false);
 
   const [alunoSelecionado, setAlunoSelecionado]=useState({
     id: '',
@@ -18,8 +19,19 @@ function App() {
     email:'',
     idade:''
   })
+
+  const selecionarAluno = (aluno, opcao) => {
+    setAlunoSelecionado(aluno);
+    (opcao === "Editar") &&
+    abrirFecharModalEditar()
+  }
+
   const abrirFecharModalIncluir=()=>{
     setModalIncluir(!modalIncluir);
+  }
+
+  const abrirFecharModalEditar=()=>{
+    setModalEditar(!modalEditar);
   }
 
   const handleChange = e=>{
@@ -49,6 +61,25 @@ function App() {
       }).catch(error=>{
         console.log(error);
       })
+  }
+
+  const pedidoPut=async()=>{
+    alunoSelecionado.idade=parseInt(alunoSelecionado.idade);
+    await axios.put(baseUrl+"/"+alunoSelecionado.id, alunoSelecionado)
+    .then(response=>{
+      var resposta=response.data;
+      var dadosAuxiliar=data;
+      dadosAuxiliar.map(aluno=>{
+        if(aluno.id===alunoSelecionado.id){
+          aluno.nome=resposta.nome;
+          aluno.email=resposta.email;
+          aluno.idade=resposta.idade;
+        }
+      });
+      abrirFecharModalEditar();
+    }).catch(error=>{
+      console.log(error);
+    })
   }
 
   useEffect(()=>{
@@ -81,8 +112,8 @@ function App() {
               <td>{aluno.email}</td>
               <td>{aluno.idade}</td>
               <td>
-                <button className='btn btn-primary' >Editar</button>{""}
-                <button className='btn btn-danger'>Excluir</button>
+                <button className='btn btn-primary' onClick={()=>selecionarAluno(aluno, "Editar")}>Editar</button>{""}
+                <button className='btn btn-danger' onClick={()=>selecionarAluno(aluno, "Editar")}>Excluir</button>
               </td>
             </tr>
           ))}
@@ -110,6 +141,38 @@ function App() {
     <ModalFooter>
       <button className="btn btn-primary"onClick={()=>pedidoPost()}>Incluir</button> {"  "}
       <button className="btn btn-danger"onClick={()=>abrirFecharModalIncluir()}>Cancelar</button>
+    </ModalFooter>
+    </Modal>
+
+
+    <Modal isOpen={modalEditar} >
+    <ModalHeader>Editar Alunos</ModalHeader>
+    <ModalBody>
+          <div className="form-group">
+          <label>ID::</label>
+          <input type="text" className="form-control" readOnly value={alunoSelecionado && alunoSelecionado.id}/> 
+            <br />
+            <br />
+            <label>Nome:</label>
+            <br />
+            <input type="text" className="form-control" name="nome" onChange={handleChange}
+            value={alunoSelecionado && alunoSelecionado.nome}/>
+            <br />
+            <label>Email:</label>
+            <br />
+            <input type="text" className="form-control" name="email" onChange={handleChange}
+            value={alunoSelecionado && alunoSelecionado.email}/>
+            <br />
+            <label>Idade:</label>
+            <br />
+            <input type="text" className="form-control" name="idade" onChange={handleChange}
+            value={alunoSelecionado && alunoSelecionado.idade}/>
+            <br />
+            </div>
+    </ModalBody>
+    <ModalFooter>
+      <button className="btn btn-primary" onClick={()=> pedidoPut()}>Editar</button> {"  "}
+      <button className="btn btn-danger"onClick={()=>abrirFecharModalEditar()}>Cancelar</button>
     </ModalFooter>
     </Modal>
 
