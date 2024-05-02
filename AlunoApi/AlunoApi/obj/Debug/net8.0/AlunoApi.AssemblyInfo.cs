92d1c93bfadf02b80776238c4baef5e70646edd1
VINIshow1907 <viniciuslucio792@gmail.com>
2024-05-02 20:35:43 -0300

    Baixei o aspNetCore.intity e foi adicionado ao projeto

--- AlunoApi/AlunoApi/obj/Debug/net8.0/AlunoApi.AssemblyInfo.cs
+++ AlunoApi/AlunoApi/obj/Debug/net8.0/AlunoApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AlunoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+579a8cc4cccde58a97c5a40ec7b8e8f90d3e4cad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a3b05473496a84b3a244c8051f67b3720a2dfbd")]
 [assembly: System.Reflection.AssemblyProductAttribute("AlunoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AlunoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
