5f031f03bc38c36135db3774d34d383cf339edcc
VINIshow1907 <viniciuslucio792@gmail.com>
2024-05-02 22:38:16 -0300

    Definindo o email e password , sign manager e user manager

--- AlunoApi/AlunoApi/Services/AuthenticateService.cs
+++ AlunoApi/AlunoApi/Services/AuthenticateService.cs
@@ -1,4 +1,5 @@
 ﻿
+using Azure.Identity;
 using Microsoft.AspNetCore.Identity;
 
 namespace AlunoApi.Services
@@ -6,15 +7,17 @@ namespace AlunoApi.Services
     public class AuthenticateService : IAuthenticate
     {
         private readonly SignInManager<IdentityUser> _signInManager;
-
-            public AuthenticateService(SignInManager<IdentityUser> signInManager)
-            {
-                _signInManager = signInManager;
-            }
+        private readonly UserManager<IdentityUser> _userInManager;
+        public AuthenticateService(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userInManager)
+        {
+            _signInManager = signInManager;
+            _userInManager = userInManager;
+        }
 
         public async Task<bool> Authenticate(string email, string password)
         {
-            var result = await _signInManager.PasswordSignInAsync(email, password, false, lockoutOnFailure: false);
+            var result = await _signInManager.PasswordSignInAsync
+                (email, password, false, lockoutOnFailure: false);
        
             return result.Succeeded;
         }
@@ -22,6 +25,23 @@ namespace AlunoApi.Services
         public async Task Logout()
         {
             await _signInManager.SignOutAsync();
+        }
+
+        public async Task<bool> RegisterUser(string email, string password)
+        {
+            var appUser = new IdentityUser
+            {
+                UserName = email,
+                Email = email,
+                
+            };
+            var result = await _userInManager.CreateAsync(appUser,password);
+
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(appUser, isPersistent: false);
+            }
+            return result.Succeeded;
         }
     }
 }
