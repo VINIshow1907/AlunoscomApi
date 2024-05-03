348df96acc21590e53a3a19c07c6cda438676928
VINIshow1907 <viniciuslucio792@gmail.com>
2024-05-02 22:37:33 -0300

    Criados dentro da pasta view, o login register model e o token de acesso

---
+++ AlunoApi/AlunoApi/ViewModels/LoginModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AlunoApi.ViewModels
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Email é obrigátorio")]
+        [EmailAddress(ErrorMessage = "Formato de email inválido")]
+
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "A senha é obrigatoria")]
+        [StringLength(20, ErrorMessage = "A {0} deve ter no minimo {2} e no máximo {1} caracteres.", MinimumLength = 10)]
+        [DataType(DataType.Password)]
+
+        public string Password { get; set; }
+    }
+}
